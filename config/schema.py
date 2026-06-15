549c9e0912a1dd630c03853204bb15ead16d4a25
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2026-06-15 23:40:34 +0330

    chore: change swagger tags

--- config/schema.py
+++ config/schema.py
@@ -3,12 +3,8 @@ from drf_spectacular.openapi import AutoSchema
 
 class AppLabelTaggedSchema(AutoSchema):
     def get_tags(self):
-        # Check for local apps in module
-        if hasattr(self.view, '__module__'):
+        if hasattr(self.view, '__module__') and "api" in self.path:
             module_parts = self.view.__module__.split('.')
-            # local_apps = ['core', 'accounts', 'competitions', 'predictions']
-            # for app_label in local_apps:
             return [module_parts[0].capitalize()]
         
-        # Fallback to default
         return super().get_tags()
--- config/urls.py
+++ config/urls.py
@@ -11,10 +11,10 @@ urlpatterns = [
 if settings.DEBUG:
     from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
     urlpatterns += [
-        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-        path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+        path('schema/', SpectacularAPIView.as_view(), name='schema'),
+        path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
         path(
-            'api/schema/redoc/',
+            'schema/redoc/',
             SpectacularRedocView.as_view(url_name='schema'),
             name='redoc-ui',
         ),
