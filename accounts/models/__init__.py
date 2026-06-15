c210658d3d52a4e521c6539eb3b4aa5435132430
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2026-06-15 15:38:56 +0330

    feat: create accounts app, user and wallet models

---
+++ accounts/models/__init__.py
@@ -0,0 +1,2 @@
+from .users import User
+from .wallets import Wallet
---
+++ accounts/models/wallets.py
@@ -0,0 +1,10 @@
+from django.db import models
+from accounts.models.users import User
+from django.core.validators import MinValueValidator
+
+class Wallet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0, validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return f"{self.user} | {self.balance}"
